301995bc34bd92b6d138ceff925f307baa7c7bed
cheun-byte <cheuntol@usc.edu>
2026-04-28 16:58:02 -0700

    newnew

--- project3/garden.js
+++ project3/garden.js
@@ -17,21 +17,36 @@ function draw() {
   background("#7084dd");
 
   for (let f of flowers) {
-    fill(f.color);
+
+    if (f.size < f.targetSize) {
+      f.size += f.growSpeed;
+    } else {
+      f.alpha -= 2; 
+    }
+
+    let c = color(f.color);
+    c.setAlpha(f.alpha);
+    fill(c);
+
     textSize(f.size);
     text(flowerSymbol, f.x, f.y);
   }
+
+  flowers = flowers.filter(f => f.alpha > 0);
 }
 
 function mousePressed() {
   for (let i = 0; i < count; i++) {
     flowers.push({
-      x: random(width),
-      y: random(height),
-      size: random(16, 64),
-      color: random(palette)
-    });
-  }
+  x: random(width),
+  y: random(height),
+  size: 0,
+  targetSize: random(32, 64),
+  growSpeed: random(0.5, 2),
+  alpha: 255,
+  hold: random(60, 180),
+  color: random(palette)
+});}
 
   count++;
 }
